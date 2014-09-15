c55465800a23d60cf3f15a665b1afc3c0d20dbdf
Stefan Vermaas <stefanvermaas1991@gmail.com>
2014-09-15 09:03:18 +0200

    Basic setup for initialization and helpers
    * Adds a really basic setup for the initialization process
    * Adds some very usefull DOM manipulation helper functions, because they're a lot faster than the jQuery alternative

--- dist/jquery.yellowtext.js
+++ dist/jquery.yellowtext.js
@@ -18,8 +18,9 @@
   function Plugin ( element, options ) {
 
     // Define the element basics
-    this.el  = element;
-    this.$el = $(this.el);
+    this.el   = element;
+    this.$el  = $(this.el);
+    this._$el = this.$el;
 
     // Define the default options
     this.options = $.extend( {}, defaults, options );
@@ -36,7 +37,115 @@
 
     // Setup the plugin for usage
     initialize: function () {
-        console.log('xD');
+
+      // Hide the current element
+      this.$el.hide();
+
+      // Create the plugin skeleton
+      this.createPluginSkeleton();
+    },
+
+    /**
+     *  createPluginSkeleton() creates the new skeleton for the text editor
+     *  after this function, the othere elements of the plugin can be
+     *  initialized.
+     *
+     *  @return element
+     */
+    createPluginSkeleton: function() {
+
+      // Create the new iframe/editor window
+    },
+
+    helper: {
+
+      /**
+       *  The following functions hasClass(), addClass(), removeClass() and
+       *  toggleClass() are borrowed from the Mozilla hack site and are used
+       *  because they can be 50-80% faster than the jQuery implementation of
+       *  these functions.
+       *
+       *  More information: https://hacks.mozilla.org/2010/01/classlist-in-firefox-3-6/
+       *  and some benchmarks: http://jsperf.com/jquery-addclass-vs-dom-classlist/10
+       */
+      hasClass: function (elm, className) {
+
+        // Validate the presence of the classList property
+        if (document.documentElement.classList) {
+          hasClass = function (elm, className) {
+            return elm.classList.contains(className);
+          };
+        } else {
+          hasClass = function (elm, className) {
+            if (!elm || !elm.className) { return false; }
+
+            var re = new RegExp('(^|\\s)' + className + '(\\s|$)');
+            return elm.className.match(re);
+          };
+        }
+
+        // Check for the given className
+        return hasClass(elm, className);
+      },
+
+      addClass: function (elm, className) {
+        if (document.documentElement.classList) {
+          addClass = function (elm, className) {
+            elm.classList.add(className);
+          };
+        } else {
+          addClass = function (elm, className) {
+            if (!elm) { return false; }
+            if (!containsClass(elm, className)) {
+                elm.className += (elm.className ? ' ' : '') + className;
+            }
+          };
+        }
+
+        // Add the given className
+        return addClass(elm, className);
+      },
+
+      removeClass: function (elm, className) {
+        if (document.documentElement.classList) {
+          removeClass = function (elm, className) {
+            elm.classList.remove(className);
+          };
+        } else {
+          removeClass = function (elm, className) {
+            if (!elm || !elm.className) { return false; }
+
+            var regexp = new RegExp('(^|\\s)' + className + '(\\s|$)', 'g');
+            elm.className = elm.className.replace(regexp, '$2');
+          };
+        }
+
+        // Remove the given className
+        return removeClass(elm, className);
+      },
+
+      toggleClass: function (elm, className) {
+        var that = this;
+
+        if (document.documentElement.classList) {
+          toggleClass = function (elm, className) {
+            return elm.classList.toggle(className);
+          };
+        } else {
+          toggleClass = function (elm, className) {
+            if (containsClass(elm, className)) {
+              that.removeClass(elm, className);
+              return false;
+            } else {
+              that.addClass(elm, className);
+              return true;
+            }
+          };
+        }
+
+        // Toggle the given className
+        return toggleClass(elm, className);
+      }
     }
   };
 
